f64da1489b28bff2fb8ce43619cf45a64a484b53
red1-web3 <red1.web3@gmail.com>
2023-04-03 02:47:40 +0600

    Main header part designed

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -16,13 +16,13 @@ function Layout({
     <>
       <Header />
       <Sidebar />
-      <div className="ml-[320px]">
+      <div className="ml-[320px] mr-[352px]">
         <main className="w-full min-h-screen px-10 py-5">
           {children}
           {isRightSideBar && (
             <div
               style={{ marginTop: headerHeight + 20 }}
-              className="fixed right-8 top-0 w-[350px] h-full bg-white dark:bg-dark-500"
+              className="fixed right-8 top-0 w-[320px] h-full bg-white dark:bg-dark-500"
             ></div>
           )}
         </main>
--- src/views/Home/Post/Post.tsx
+++ src/views/Home/Post/Post.tsx
@@ -1,7 +1,31 @@
+import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 function Post() {
-  return <article className="rounded-md">Post</article>;
+  return (
+    <article className="rounded-md _border dark:bg-dark-500 p-3">
+      <header className="grid grid-cols-[40px,auto] gap-x-2 items-center">
+        <Link href={"#"}>
+          <Image
+            src={"/profile-img.jpg"}
+            height={40}
+            width={40}
+            alt="Profile image"
+            className="object-cover rounded-full aspect-square"
+          />
+        </Link>
+
+        <div>
+          <input
+            type="text"
+            className="w-full dark:bg-dark-400 outline-none dark:text-slate-200 text-black placeholder:opacity-50 rounded-full py-2 px-5"
+            placeholder="What's on your mind?"
+          />
+        </div>
+      </header>
+    </article>
+  );
 }
 
 export default Post;
